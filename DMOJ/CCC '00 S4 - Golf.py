2c35649afd48b8a9fee8445ac7337251ac30e675
Makan Dey <makan@mastacoder.com>
2019-02-16 21:30:54 -0500

    Whoops, old TLE solution

--- DMOJ/CCC '00 S4 - Golf.py
+++ DMOJ/CCC '00 S4 - Golf.py
@@ -1,45 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-DEBUG = False
-
 length = int(input())
 amount = int(input())
 clubs = [int(input()) for a in range(amount)]
-q = clubs[:]
-q.append(None)
-c = 1
-al = []
+l = [1000 for n in range(length + 1)]
+l[0] = 0
 
-def addnextsum(p):
-    if DEBUG:
-        print q
-    for a in range(amount):
-        np = p + clubs[a]
-        if np == length:
-            return True
-        elif not np in al and np <= length:
-            q.append(np)
-            al.append(np)
-    return False
+for a in range(length):
+    for b in clubs:
+        t = [b + a, l[a] + 1]
+        if t[0] <= length and l[t[0]] > t[1]:
+            l[t[0]] = t[1]
 
-while True:
-    on = q.pop(0)
-    if on == length:
-        break
-    elif on == None:
-        if len(q) > 1:
-            c += 1
-            q.append(None)
-        else:
-            c = -1
-            break
-    else:
-        if addnextsum(on):
-            c += 1
-            break
-
-if c > 0:
-    print "Roberta wins in", c, "strokes."
+if l[-1] != 1000:
+    print "Roberta wins in", l[-1], "strokes."
 else:
     print "Roberta acknowledges defeat."
