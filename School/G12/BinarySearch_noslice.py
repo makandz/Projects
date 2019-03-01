c8a1888a2d1e60b84548b782e06705a0fcc0595e
Makan Dey <makan@mastacoder.com>
2019-03-01 11:01:01 -0500

    Update BinarySearch_noslice.py

--- School/G12/BinarySearch_noslice.py
+++ School/G12/BinarySearch_noslice.py
@@ -1,38 +1,44 @@
 import random, math
 
-# CONFIGURATION
-amount = 1000
-maximum = 100
-look = 10
-
-# Create and sort
-numbers = [random.randint(1, 100) for n in range(amount)]
-numbers.sort()
-
-# Split the start
-cur = [0, amount // 2, amount - 1] # start, middle, end
-
-while 1: # Start looping
-    if numbers[cur[0]] == look: # is the start found?
-        print(cur[0])
-        break
-    elif numbers[cur[2]] == look: # is the end found?
-        print(cur[2])
-        break
-    elif numbers[cur[1]] == look: # is the middle found?
-        print(cur[1])
-        break
-    elif cur[1] == cur[2] or cur[1] == cur[0]: # Is there no middle?
-        print("Failed to find the number.")
-        break
+def binary_search(numbers, look):
+    """Binary Search algorithm"""
     
-    # Split the list.
-    if numbers[cur[1]] > look: # Split from the start, to middle.
-        cur[2] = cur[1] # Set end to middle
-        cur[1] = int(cur[2] / 2) # Find the new middle
-    else:
-        cur[0] = cur[1] # Set start to middle
-        cur[1] = int(cur[0] + ((cur[2] - cur[1]) / 2)) # Find the new middle
+    # Split the start
+    start = 0
+    mid = len(numbers) // 2
+    end = len(numbers) - 1
+    found = -2
+
+    def check_val(_start, _mid, _end):
+        """Checks if the value is equal"""
+        if numbers[_start] == look: # is the start found?
+            return _start
+        elif numbers[_end] == look: # is the end found?
+            return _end
+        elif numbers[_mid] == look: # is the middle found?
+            return _mid
+        elif _mid == _end or _mid == _start: # Is there no middle?
+            return -1
+        else:
+            return -2 # Nothing found
+
+    while 1: # Start looping
+        found = check_val(start, mid, end) # do the check
+        if found > -2:
+            break
+        
+        # Split the list.
+        if numbers[mid] > look: # Split from the start, to middle.
+            end = mid # Set end to middle
+            mid = int(end / 2) # Find the new middle
+        else:
+            start = mid # Set start to middle
+            mid = int(start + ((end - mid) / 2)) # Find the new middle
+
+    return found
+
+# make da numbers
+_numbers = sorted([random.randint(1, 10) for n in range(10)])
 
 # Printing
-print(numbers)
+print(_numbers, binary_search(_numbers, 8))
