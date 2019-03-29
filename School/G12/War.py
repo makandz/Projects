4f23277ff56b187f47ec57d31d75ef9462e0a918
Makan Dey <makan@mastacoder.com>
2019-03-29 11:35:25 -0400

    Update War.py

--- School/G12/War.py
+++ School/G12/War.py
@@ -46,11 +46,60 @@ class Deck():
         """deal a card from the top of the deck"""
         return self.cards.pop(-1)
 
-# CREATE A DECK OF CARDS
-# -----------------------
+    def amount(self):
+        """return the amount of cards in a deck"""
+        return len(self.cards)
 
+# Create the center deck of all cards.
 deck = Deck()
 for a in ranks:
     for b in suits:
-        t_ = Card(b, a)
-        deck.add_card(t_)
+        deck.add_card(Card(b, a))
+
+deck.shuffle() # shuffle the deck
+
+# Start sending cards to each deck.
+D1 = Deck()
+D2 = Deck()
+you = False
+for card in deck.cards:
+    you = not you
+    if you: # pass to each user
+        D1.add_card(card)
+    else:
+        D2.add_card(card)
+deck.cards = [] # empty center
+
+print("----------------------------------------------")
+print("WELCOME TO WAR                 MADE BY MAKAN D")
+print("----------------------------------------------")
+
+rnd = 1
+war = False
+D1_owned = Deck()
+D2_owned = Deck()
+print("[GAME] Currently on round: 1")
+while 1:
+    center = []
+    choice = input("$ ")
+    if choice == "amount":
+        print("Your Holding Hand:", D1.amount(), "- Deck:", D1_owned.amount())
+    elif choice == "opp amount":
+        print("Opponent Holding Hand:", D2.amount(), "- Deck:", D2_owned.amount())
+    elif choice == "draw":
+        if not war: # no war
+            center.append(D1.deal_one()) # deal from you
+            center.append(D2.deal_one()) # deal from opponent
+
+            # both deal comparison cards
+            c1 = D1.deal_one()
+            c2 = D2.deal_one()
+
+            # comapre the ranks
+            rank = c1.compare(c2)
+            if rank == -1: # lost
+                print("[LOST] Your", c1, "got destroyed by", c2)
+            elif rank == 0:
+                print("[WAR] You're at war with", c1, "and", c2)
+            else:
+                 print("[WON] Your", c1, "destroyed", c2)
